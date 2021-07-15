d112241b37d6515b625777f6330e11a729fc8a44
Kiran <kiran.extreme@outlook.com>
2021-07-15 17:43:17 +0530

    Entry list of the files

---
+++ src/app/blog/entry-list/entry-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { entries, Post } from 'config/entries';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+  entries: Post[] = [];
+
+  constructor() { }
+
+  slugify(text: string): string {
+    return text
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, ''); // Trim - from end of text
+  }
+
+  ngOnInit(): void {
+    this.entries = entries.map((entry) => ({
+      ...entry,
+      slug: this.slugify(entry.title),
+    }));
+  }
+
+}
