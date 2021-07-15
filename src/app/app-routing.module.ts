1b276dca816771b06483d93b8d539f3b74185f46
Kiran <kiran.extreme@outlook.com>
2021-07-15 17:42:33 +0530

    Added blog modules in the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { // lazy load the blog component
+    path: '',
+    loadChildren: () =>
+      import('./blog/blog.module').then((m) => m.BlogModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxMdModule } from 'ngx-md';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMdModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
