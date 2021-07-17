b2412c2a8d9cb87cc32511ccbb41f3384bd65a66
Kiran <kiran.extreme@outlook.com>
2021-07-18 00:51:54 +0530

    Added material card

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,18 +3,21 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
     imports: [
         MatSidenavModule,
         MatToolbarModule,
         MatIconModule,
-        MatListModule],
+        MatListModule,
+        MatCardModule],
     exports: [
         MatSidenavModule,
         MatToolbarModule,
         MatIconModule,
-        MatListModule]
+        MatListModule,
+        MatCardModule]
 })
 
 export class MaterialModule {}
