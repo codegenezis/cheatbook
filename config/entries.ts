96c50be5054b55ad0e47aa9738cfd6535ea6c325
Kiran <kiran.extreme@outlook.com>
2021-07-18 02:34:06 +0530

    Added image in the entry file

--- config/entries.ts
+++ config/entries.ts
@@ -3,27 +3,29 @@ export interface Post {
     title: string;
     slug?: string;
     date: Date;
+    image: string;
   }
   
   export const entries: Post[] = [
     {
       title: 'bash-cheatsheet',
       date: new Date('2021-01-27'),
+      image: '/assets/Bash.jpeg'
     },
     {
       title: 'python-cheatsheet',
       date: new Date('2020-12-23'),
+      image: '/assets/python.png'
     },
     {
       title: 'git-cheatsheet',
-      date: new Date('2020-10-11')
+      date: new Date('2020-10-11'),
+      image: '/assets/git.jpg'
     },
     {
       title: 'docker-cheatsheet',
-      date: new Date('2020-10-11')
+      date: new Date('2020-10-11'),
+      image: '/assets/docker.png'
     },
-    {
-      title: 'Ansible-cheatsheet',
-      date: new Date('2020-10-11')
-    }
+
   ];
