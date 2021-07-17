50c880ad4364d36400cd63b1a51b30e81892e2b7
Kiran <kiran.extreme@outlook.com>
2021-07-17 21:42:52 +0530

    Added Header and sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgxMdModule } from 'ngx-md';
+// import { NgxMdModule } from 'ngx-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxMdModule.forRoot(),
+    // NgxMdModule.forRoot(),
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
